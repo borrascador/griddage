aa233a4dc958ee4a0537c90fd5cf43665184c3b2
borrascador <roflbot101@gmail.com>
2016-06-07 14:05:48 +0800

    Dummy settings page implemented

--- griddage.py
+++ griddage.py
@@ -5,11 +5,14 @@ from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.graphics import Color, Rectangle
 from kivy.animation import Animation
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.image import Image
+from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
 from kivy.properties import \
      NumericProperty, ReferenceListProperty, StringProperty, ObjectProperty
 
@@ -84,7 +87,7 @@ class Board(FloatLayout):
                self.game.current_player == self.game.players[2]:
                 pos_iter  = iter((x+0.5, 0.5) for x in range(1,5))
                 size_iter = iter((1, (6-(1-SIZE_FACTOR))/SIZE_FACTOR) \
-                                  for i in range(4))
+                                      for i in range(4))
             else:
                 pos_iter  = iter((0.5, y+0.5) for y in range(1,5))
                 size_iter = iter(((6-(1-SIZE_FACTOR))/SIZE_FACTOR, 1) \
@@ -143,7 +146,7 @@ class Board(FloatLayout):
                                         xpos=3, ypos=0)
             self.add_widget(self.solo_label)
             
-        if len(self.game.players) == 2:
+        elif len(self.game.players) == 2:
             self.col_label = GridLabel(text='[b]%d[/b]' % \
                                        self.game.players[0].score,
                                        markup=True, font_size=20,
@@ -156,7 +159,7 @@ class Board(FloatLayout):
                                        xpos=4, ypos=0)
             self.add_widget(self.row_label)
             
-        if len(self.game.players) == 4:
+        elif len(self.game.players) == 4:
             self.col_label = GridLabel(text='[b]%d[/b]' % \
                                        self.game.players[0].score,
                                        markup=True, font_size=20,
@@ -180,8 +183,8 @@ class Board(FloatLayout):
                 
         self.game.current_player.cards[-1].flip()
 
-        if self.game.players[1].name == "Bot1" and \
-           self.game.current_player.name == "Bot1":
+        if len(self.game.players) == 2 and self.game.players[1].name == "Bot1" \
+        and self.game.current_player.name == "Bot1":
             self.computer_turn()
 
     def update_score(self, *args):
@@ -199,8 +202,8 @@ class Board(FloatLayout):
         anim.bind(on_complete=self.update_background)
         if self.game.is_round_over():
             anim.bind(on_complete=self.end_round)
-        elif self.game.players[1].name == "Bot1" and \
-             self.game.current_player.name != "Bot1":
+        elif len(self.game.players)==2 and self.game.players[1].name=="Bot1" \
+        and self.game.current_player.name != "Bot1":
             anim.bind(on_complete=self.computer_turn)
         anim.start(instance)
         
@@ -260,7 +263,7 @@ class GameScreen(Screen):
 
         self.bind(on_pre_enter=self.build)
 
-    def build(self, *args):        
+    def build(self, *args):
         if   self.manager.mode == 'Solitaire Mode':
             self.game = Game(['Anna'])
         elif self.manager.mode == '2 Player Mode':
@@ -283,6 +286,86 @@ class GameScreen(Screen):
 
     def goto_menu(self, *args):
         self.remove_widget(self.board)
+        self.manager.current = 'menu_screen'
+
+
+
+class SettingsLyt(StackLayout):
+    def __init__(self, **kwargs):
+        super(SettingsLyt, self).__init__(**kwargs)
+        
+        with self.canvas.before:
+            Color(0, 0.6, 0)
+            self.base_rect = Rectangle(size=self.size,
+                                            pos=self.pos)
+            self.bind(size=self.update_background,
+                      pos=self.update_background)
+
+        self.orientation,self.spacing,self.padding = ('lr-tb', [20,20], [20,20])
+
+    def update_background(self, *args):
+        self.base_rect.size = self.size
+        self.base_rect.pos  = self.pos
+        
+
+
+class SettingsScreen(Screen):
+    def __init__(self, **kwargs):
+        super(SettingsScreen, self).__init__(**kwargs)
+
+        self.settings = SettingsLyt()
+        self.add_widget(self.settings)
+        self.make_widgets()
+
+    def make_widgets(self):
+        button_names = ('Edit Names', self.popup_names), \
+                       ('Victory Score', self.popup_score), \
+                       ('Back to Menu', self.back_to_menu)
+
+        for name in button_names:
+            button = Button(text=name[0], font_size=50, size_hint=(1,None))
+            button.bind(on_press=name[1])
+            self.settings.add_widget(button)
+
+    def popup_names(self, button):
+        popup_layout = StackLayout(orientation='tb-lr',
+                                   spacing=[20,20],
+                                   padding=[20,20])
+        
+        for num in range(1,5):
+            label_text = 'Player ' + str(num)
+            label = Label(text=label_text, size_hint=(0.5,None), height=30)
+            popup_layout.add_widget(label)
+            
+        names = ['Anna', 'Jan', 'Garret', 'Johno']
+        for name in names:
+            name_input = TextInput(text=name, multiline=False,
+                                   size_hint=(0.5,None), height=30)
+            popup_layout.add_widget(name_input)
+        
+        popup = Popup(title='Edit Names',
+                      content=popup_layout, 
+                      size_hint=(0.8,None), height=290)
+        popup.open()
+
+    def popup_score(self, button):
+        popup_layout = StackLayout(orientation='tb-lr',
+                                   spacing=[20,20],
+                                   padding=[20,20])
+        
+        label = Label(text='Victory Score', size_hint=(0.5,None), height=30)
+        popup_layout.add_widget(label)
+            
+        score_input = TextInput(text=str(61), multiline=False,
+                               size_hint=(0.5,None), height=30)
+        popup_layout.add_widget(score_input)
+        
+        popup = Popup(title='Edit Names',
+                      content=popup_layout, 
+                      size_hint=(0.8,None), height=150)
+        popup.open()
+        
+    def back_to_menu(self, button):
         self.manager.current = 'menu_screen'
 
 
@@ -326,10 +409,11 @@ class MenuScreen(Screen):
         
     def change(self, button):
         self.manager.mode = button.text
-        if self.manager.mode == 'Settings':
+        if button.text == 'Settings':
             self.manager.current = 'settings_screen'
         else:
             self.manager.current = 'game_screen'
+
 
         
 
@@ -345,8 +429,8 @@ class GriddageApp(App):
         self.game_screen = GameScreen(name='game_screen')
         self.manager.add_widget(self.game_screen)
 
-##        self.settings_screen = SettingsScreen(name='settings_screen')
-##        self.manager.add_widget(self.game_screen)
+        self.settings_screen = SettingsScreen(name='settings_screen')
+        self.manager.add_widget(self.settings_screen)
         
         return self.manager
 
